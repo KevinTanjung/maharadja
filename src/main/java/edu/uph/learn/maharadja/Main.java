e366db015c264656a5dc729d79c59e24c3ba51bf
Kevin Tanjung <kevin.tanjung@traveloka.com>
2025-03-22 21:44:00 +0700

    feat: allow running server and client in debug mode

--- src/main/java/edu/uph/learn/maharadja/Main.java
+++ src/main/java/edu/uph/learn/maharadja/Main.java
@@ -2,6 +2,15 @@ package edu.uph.learn.maharadja;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    if (args.length == 2 && args[0].equals("--server")) {
+      // TODO: run as server mode
+      System.out.println("Running as server in debug mode as: " + args[1]);
+    } else if (args.length == 2 && args[0].equals("--client")) {
+      // TODO: run as client mode
+      System.out.println("Running as client in debug mode as: " + args[1]);
+    } else {
+      System.out.println("Launching Maharadja UI...");
+      // TODO: run as UI mode
+    }
   }
 }
