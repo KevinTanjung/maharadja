387fb783a0fce185f100e148e8fdebc35e51d3e2
Kevin Tanjung <kevin.tanjung@traveloka.com>
2025-03-22 21:53:31 +0700

    feat: change logging implementation to use log4j2

--- src/main/java/edu/uph/learn/maharadja/Main.java
+++ src/main/java/edu/uph/learn/maharadja/Main.java
@@ -1,15 +1,20 @@
 package edu.uph.learn.maharadja;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
     if (args.length == 2 && args[0].equals("--server")) {
       // TODO: run as server mode
-      System.out.println("Running as server in debug mode as: " + args[1]);
+      LOG.info("Running as server in debug mode as: {}", args[1]);
     } else if (args.length == 2 && args[0].equals("--client")) {
       // TODO: run as client mode
-      System.out.println("Running as client in debug mode as: " + args[1]);
+      LOG.info("Running as client in debug mode as: {}", args[1]);
     } else {
-      System.out.println("Launching Maharadja UI...");
+      LOG.info("Launching Maharadja UI...");
       // TODO: run as UI mode
     }
   }
